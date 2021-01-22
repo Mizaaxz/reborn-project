795985f398921cc2cf2e566f7d869980325e04cf
Meow <ez@clap.com>
2021-01-22 12:11:20 -0500

    add port

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import url from 'url';
-import http from 'http';
-import dotenv from 'dotenv';
-import express from 'express';
-import nunjucks from 'nunjucks';
-import SHA256 from 'fast-sha256';
-import arrayBufferToHex from 'array-buffer-to-hex';
+import url from "url";
+import http from "http";
+import dotenv from "dotenv";
+import express from "express";
+import nunjucks from "nunjucks";
+import SHA256 from "fast-sha256";
+import arrayBufferToHex from "array-buffer-to-hex";
 
-import * as console from './console';
-import { Server as WSServer } from 'ws';
-import UptimeWSServer from './uptimeWS';
-import { startServer } from './moomoo/moomoo';
-import { getGame } from './moomoo/Game';
-import { TextEncoder } from 'util';
+import * as console from "./console";
+import { Server as WSServer } from "ws";
+import UptimeWSServer from "./uptimeWS";
+import { startServer } from "./moomoo/moomoo";
+import { getGame } from "./moomoo/Game";
+import { TextEncoder } from "util";
 
 const app = express();
 const server = http.createServer(app);
@@ -19,44 +19,54 @@ const port = process.env.PORT;
 
 dotenv.config();
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
-  express: app
+  express: app,
 });
 
 const VERSION = "0.0.0a";
 
 function format(timestamp: number) {
   var hours = Math.floor(timestamp / (60 * 60));
-  var minutes = Math.floor(timestamp % (60 * 60) / 60);
+  var minutes = Math.floor((timestamp % (60 * 60)) / 60);
   var seconds = Math.floor(timestamp % 60);
 
-  return hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+  return (
+    hours.toString().padStart(2, "0") +
+    ":" +
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0")
+  );
 }
 
-app.get('/sanctuary', (req, res) => {
-  if (req.accepts('html')) {
-    res.render('version.html', { version: VERSION, nodeVersion: process.version, uptime: format(process.uptime()) });
+app.get("/sanctuary", (req, res) => {
+  if (req.accepts("html")) {
+    res.render("version.html", {
+      version: VERSION,
+      nodeVersion: process.version,
+      uptime: format(process.uptime()),
+    });
     return;
   }
 
-  res.send('Sanctuary v${VERSION}');
+  res.send("Sanctuary v${VERSION}");
 });
 
-app.get('/uptime', (req, res) => {
-  if (req.accepts('html')) {
-    res.redirect('/sanctuary');
+app.get("/uptime", (req, res) => {
+  if (req.accepts("html")) {
+    res.redirect("/sanctuary");
     return;
   }
 
   res.send(format(process.uptime()));
 });
 
-app.get('/', (req, res) => {
-  res.redirect(`${req.protocol}://moomoo.io`)
+app.get("/", (req, res) => {
+  res.redirect(`${req.protocol}://moomoo.io`);
 });
 
-app.get('/api/v1/playerCount', (_req, res) => {
+app.get("/api/v1/playerCount", (_req, res) => {
   let game = getGame();
 
   if (!game) {
@@ -66,22 +76,20 @@ app.get('/api/v1/playerCount', (_req, res) => {
   }
 });
 
-app.get('/api/v1/players', (req, res) => {
+app.get("/api/v1/players", (req, res) => {
   let game = getGame();
 
   if (!game) {
     res.send(JSON.stringify({ type: "error", message: "No game active." }));
   } else {
-    let clients: { clientIPHash: string, playerName: string, playerID: number }[] = [];
+    let clients: { clientIPHash: string; playerName: string; playerID: number }[] = [];
 
     for (let client of game.clients) {
-      clients.push(
-        {
-          clientIPHash: arrayBufferToHex(SHA256(new TextEncoder().encode(client.ip))),
-          playerName: client.player?.name || "unknown",
-          playerID: client.player?.id || -1
-        }
-      );
+      clients.push({
+        clientIPHash: arrayBufferToHex(SHA256(new TextEncoder().encode(client.ip))),
+        playerName: client.player?.name || "unknown",
+        playerID: client.player?.id || -1,
+      });
     }
 
     res.send(JSON.stringify({ type: "success", clients: clients }));
@@ -95,16 +103,16 @@ let uptimeServer = new WSServer({ noServer: true });
 
 new UptimeWSServer(uptimeServer);
 
-server.on('upgrade', function upgrade(request, socket, head) {
-  const pathname = url.parse(request.url).pathname?.replace(/\/$/, '');
+server.on("upgrade", function upgrade(request, socket, head) {
+  const pathname = url.parse(request.url).pathname?.replace(/\/$/, "");
 
-  if (pathname === '/uptimeWS') {
+  if (pathname === "/uptimeWS") {
     uptimeServer.handleUpgrade(request, socket, head, function done(ws) {
-      uptimeServer.emit('connection', ws, request);
+      uptimeServer.emit("connection", ws, request);
     });
-  } else if (pathname === '/moomoo') {
+  } else if (pathname === "/moomoo") {
     wss.handleUpgrade(request, socket, head, function done(ws) {
-      wss.emit('connection', ws, request);
+      wss.emit("connection", ws, request);
     });
   } else {
     socket.destroy();
@@ -113,4 +121,6 @@ server.on('upgrade', function upgrade(request, socket, head) {
 
 console.startConsole();
 
-server.listen(port || 3000, () => console.log(`Sanctuary listening at https://localhost:${port || 3000}`));
+server.listen(port || 6500, () =>
+  console.log(`Sanctuary listening at https://localhost:${port || 3000}`)
+);
