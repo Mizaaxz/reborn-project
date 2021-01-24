0c3d67590c913b04da7c9bf626e2ac1a24e29512
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-24 17:35:30 -0500

    change to player

--- src/functions.ts
+++ src/functions.ts
@@ -1,48 +1,48 @@
 import { WeaponVariant } from "./moomoo/Weapons";
-
-const setWeaponVariant = function(thisPlayer: any, variant: any) {
+const setWeaponVariant = function(player: any, variant: any) {
   switch (variant) {
               case "emerald":
               case "em":
               case "e":
-                thisPlayer.selectedWeapon === thisPlayer.weapon
-                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Emerald)
-                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Emerald);
+                player.selectedWeapon === player.weapon
+                  ? (player.primaryWeaponVariant = WeaponVariant.Emerald)
+                  : (player.secondaryWeaponVariant = WeaponVariant.Emerald);
                 break;
                 
               case "ruby":
               case "r":
-                thisPlayer.selectedWeapon === thisPlayer.weapon
-                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Ruby)
-                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Ruby);
+                player.selectedWeapon === player.weapon
+                  ? (player.primaryWeaponVariant = WeaponVariant.Ruby)
+                  : (player.secondaryWeaponVariant = WeaponVariant.Ruby);
                 break;
 
               case "diamond":
               case "dia":
               case "d":
-                thisPlayer.selectedWeapon === thisPlayer.weapon
-                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Diamond)
-                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Diamond);
+                player.selectedWeapon === player.weapon
+                  ? (player.primaryWeaponVariant = WeaponVariant.Diamond)
+                  : (player.secondaryWeaponVariant = WeaponVariant.Diamond);
                 break;
 
               case "gold":
               case "g":
-                thisPlayer.selectedWeapon === thisPlayer.weapon
-                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Gold)
-                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Gold);
+                player.selectedWeapon === player.weapon
+                  ? (player.primaryWeaponVariant = WeaponVariant.Gold)
+                  : (player.secondaryWeaponVariant = WeaponVariant.Gold);
                 break;
 
               case "normal":
               case "norm":
               case "default":
               case "reset":
-                thisPlayer.selectedWeapon === thisPlayer.weapon
-                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Normal)
-                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Normal);
+                player.selectedWeapon === player.weapon
+                  ? (player.primaryWeaponVariant = WeaponVariant.Normal)
+                  : (player.secondaryWeaponVariant = WeaponVariant.Normal);
                 break;
 
               default:
                 return 1;
             }
+
 }
 export { setWeaponVariant };
