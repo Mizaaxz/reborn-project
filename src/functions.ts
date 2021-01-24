dc2458f7c7378fbc65af90cb3a6d785cbf7fb08f
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-24 17:12:45 -0500

    Create functions.ts

---
+++ src/functions.ts
@@ -0,0 +1,46 @@
+const setWeaponVariant = function(player, variant) {
+  switch (variant) {
+              case "emerald":
+              case "em":
+              case "e":
+                thisPlayer.selectedWeapon === thisPlayer.weapon
+                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Emerald)
+                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Emerald);
+                break;
+                
+              case "ruby":
+              case "r":
+                thisPlayer.selectedWeapon === thisPlayer.weapon
+                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Ruby)
+                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Ruby);
+                break;
+
+              case "diamond":
+              case "dia":
+              case "d":
+                thisPlayer.selectedWeapon === thisPlayer.weapon
+                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Diamond)
+                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Diamond);
+                break;
+
+              case "gold":
+              case "g":
+                thisPlayer.selectedWeapon === thisPlayer.weapon
+                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Gold)
+                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Gold);
+                break;
+
+              case "normal":
+              case "norm":
+              case "default":
+              case "reset":
+                thisPlayer.selectedWeapon === thisPlayer.weapon
+                  ? (thisPlayer.primaryWeaponVariant = WeaponVariant.Normal)
+                  : (thisPlayer.secondaryWeaponVariant = WeaponVariant.Normal);
+                break;
+
+              default:
+                return 1;
+            }
+}
+export default setWeaponVariant
