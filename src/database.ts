b02cd3ddac603eeb91f3d6ee3a3995e3913e4ff2
Meow <ez@clap.com>
2021-01-29 16:42:37 -0500

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,36 +1,36 @@
 import fs from "fs";
 let db = {
   fetch: (key: string) => {
-    return require("../db.json")[key];
+    return require("../.db.json")[key];
   },
   add: (key: string, value: any) => {
-    let data = require("../db.json");
+    let data = require("../.db.json");
     data[key] = data[key] || 0;
     data[key] += value;
-    fs.writeFileSync("../db.json", JSON.stringify(data));
+    fs.writeFileSync("../.db.json", JSON.stringify(data));
     return data;
   },
   subtract: (key: string, value: any) => {
-    let data = require("../db.json");
+    let data = require("../.db.json");
     data[key] = data[key] || 0;
     data[key] -= value;
-    fs.writeFileSync("../db.json", JSON.stringify(data));
+    fs.writeFileSync("../.db.json", JSON.stringify(data));
     return data;
   },
   set: (key: string, value: any) => {
-    let data = require("../db.json");
+    let data = require("../.db.json");
     data[key] = value;
-    fs.writeFileSync("../db.json", JSON.stringify(data));
+    fs.writeFileSync("../.db.json", JSON.stringify(data));
     return data;
   },
   delete: (key: string) => {
-    let data = require("../db.json");
+    let data = require("../.db.json");
     delete data[key];
-    fs.writeFileSync("../db.json", JSON.stringify(data));
+    fs.writeFileSync("../.db.json", JSON.stringify(data));
     return data;
   },
   all: () => {
-    let data = require("../db.json");
+    let data = require("../.db.json");
     return data;
   },
   get: Function(),
