93ff96bc8976e9ab7036e91cef4d0bfb9ac539f9
Meow <ez@clap.com>
2021-01-29 17:17:42 -0500

    fix

--- src/database.ts
+++ src/database.ts
@@ -1,36 +1,36 @@
 import fs from "fs";
 let db = {
   fetch: (key: string) => {
-    return require(__dirname + "\\..\\.db.json")[key];
+    return require(__dirname + "\\..\\data.json")[key];
   },
   add: (key: string, value: any) => {
-    let data = require(__dirname + "\\..\\.db.json");
+    let data = require(__dirname + "\\..\\data.json");
     data[key] = data[key] || 0;
     data[key] += value;
-    fs.writeFile(__dirname + "\\..\\.db.json", JSON.stringify(data), function () {});
+    fs.writeFile(__dirname + "\\..\\data.json", JSON.stringify(data), function () {});
     return data;
   },
   subtract: (key: string, value: any) => {
-    let data = require(__dirname + "\\..\\.db.json");
+    let data = require(__dirname + "\\..\\data.json");
     data[key] = data[key] || 0;
     data[key] -= value;
-    fs.writeFile(__dirname + "\\..\\.db.json", JSON.stringify(data), function () {});
+    fs.writeFile(__dirname + "\\..\\data.json", JSON.stringify(data), function () {});
     return data;
   },
   set: (key: string, value: any) => {
-    let data = require(__dirname + "\\..\\.db.json");
+    let data = require(__dirname + "\\..\\data.json");
     data[key] = value;
-    fs.writeFile(__dirname + "\\..\\.db.json", JSON.stringify(data), function () {});
+    fs.writeFile(__dirname + "\\..\\data.json", JSON.stringify(data), function () {});
     return data;
   },
   delete: (key: string) => {
-    let data = require(__dirname + "\\..\\.db.json");
+    let data = require(__dirname + "\\..\\data.json");
     delete data[key];
-    fs.writeFile(__dirname + "\\..\\.db.json", JSON.stringify(data), function () {});
+    fs.writeFile(__dirname + "\\..\\data.json", JSON.stringify(data), function () {});
     return data;
   },
   all: () => {
-    let data = require(__dirname + "\\..\\.db.json");
+    let data = require(__dirname + "\\..\\data.json");
     return data;
   },
   get: Function(),
