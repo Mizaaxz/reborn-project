60b26cd7d23ba08fc88af4f87663b14c72229fc0
Meow <ez@clap.com>
2021-02-03 10:40:06 -0500

    add

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,6 @@
 import Command from "../classes/Command";
 import Discord from "discord.js";
+import ms from "ms";
 
 const cmd = new Command(
   "ping",
@@ -10,7 +11,10 @@ const cmd = new Command(
     adminOnly: false,
   },
   function (bot: Discord.Client, message: Discord.Message, args: any[]) {
-    message.channel.send("P0nG");
+    let embed = new Discord.MessageEmbed();
+    embed.setAuthor(`${bot.user?.username} Ping`, bot.user?.displayAvatarURL());
+    embed.addField("API Ping", ms(bot.ws.ping), true);
+    message.channel.send(embed);
   }
 );
 module.exports = cmd;
