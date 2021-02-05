fe8a29599afaec35dff1ea681a719a610be43968
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-02-04 20:43:28 -0500

    Update xp.ts

--- src/bot/modules/xp.ts
+++ src/bot/modules/xp.ts
@@ -2,15 +2,12 @@ import Discord from "discord.js";
 import db from "../../database";
 
 let Levels = {
-  1000: {
-    name: "Food",
-    emoji: 802944418671362138,
-  },
-  2500: { name: "Wood", emoji: 802944470719004702 },
-  5000: { name: "Stone", emoji: 802944460031524874 },
-  7500: { name: "Gold", emoji: 802944433704271913 },
-  10000: { name: "Killer", emoji: 802944444370255882 },
-  Infinity: { name: "GODLIKE", emoji: 802944408193597440 },
+  1000: { name: "Food", emoji: "802944418671362138", role: "" },
+  2500: { name: "Wood", emoji: "802944470719004702", role: "" },
+  5000: { name: "Stone", emoji: "802944460031524874", role: "" },
+  7500: { name: "Gold", emoji: "802944433704271913", role: "" },
+  10000: { name: "Killer", emoji: "802944444370255882", role: "" },
+  Infinity: { name: "GODLIKE", emoji: "802944408193597440", role: "" },
 };
 
 function levelUp(user: Discord.User) {
