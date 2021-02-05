b0875eb0d574d0b70a011417038ac188aa443381
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-02-04 22:56:26 -0500

    Update players.ts

--- src/bot/commands/players.ts
+++ src/bot/commands/players.ts
@@ -20,6 +20,14 @@ const cmd = new Command(
       `Listing ${clients.length} people online.`,
       message.guild?.iconURL() || undefined
     );
+
+    let players = "";
+    clients.forEach((c) => {
+      players += `**Name:** ${c.playerName}
+**ID:** ${c.playerID == -1 ? "In Lobby" : c.playerID}`;
+    });
+    embed.setDescription(players || "No Players Online");
+
     message.channel.send(embed);
   }
 );
