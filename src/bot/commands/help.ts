dfa4c5f64fe615aeb70fe586894d03d6b1e39d37
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-02-07 21:26:45 -0500

    add

---
+++ src/bot/commands/help.ts
@@ -0,0 +1,21 @@
+import Command from "../Command";
+import Discord from "discord.js";
+import ms from "ms";
+import fs from "fs";
+
+const cmd = new Command(
+  "help",
+  {
+    description: "Displays this menu!",
+    usage: "help <command>",
+    aliases: ["commands", "cmds"],
+    required: (mem: Discord.GuildMember) => {
+      return true;
+    },
+  },
+  async function (bot: Discord.Client, message: Discord.Message, args: any[]) {
+    let commands = fs.readdirSync("commands");
+    message.channel.send(JSON.stringify(commands));
+  }
+);
+module.exports = cmd;
