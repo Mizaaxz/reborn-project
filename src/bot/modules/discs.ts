bdfc88dfd8732fdff9608b9f9beee9bafdfaeea0
Meow <ez@clap.com>
2021-02-10 13:16:39 -0500

    add

--- src/bot/modules/discs.ts
+++ src/bot/modules/discs.ts
@@ -11,6 +11,7 @@ const discs: any = {
   corpse_missyou: "https://www.youtube.com/watch?v=clnIeauiVDM",
   corpse_neversatisfied: "https://www.youtube.com/watch?v=cCc7yTpRGcI",
   corpse_whitetee: "https://www.youtube.com/watch?v=yS6aXxdL6M8",
+  gotbitches: "https://www.youtube.com/watch?v=mdqU6Erw3kk",
   lofi: "https://www.youtube.com/watch?v=5qap5aO4i9A",
   morshu: "https://www.youtube.com/watch?v=cJs7r0TLfx0",
   pigstep_remix: "https://www.youtube.com/watch?v=r_a4Zg3PyA0",
