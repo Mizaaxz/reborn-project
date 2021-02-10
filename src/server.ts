b2571c5d1b1527abe6f7fa36bb20971f3dd033dc
Meow <ez@clap.com>
2021-02-10 18:54:00 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -59,7 +59,8 @@ function format(timestamp: number) {
 }
 
 app.use((req, res, next) => {
-  if (req.headers.origin) res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  //if (req.headers.origin) res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
