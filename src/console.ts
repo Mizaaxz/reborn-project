3614e86eac31013fbc6f43629f20193c57be753b
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-03-05 10:16:47 -0500

    fix

--- src/console.ts
+++ src/console.ts
@@ -366,11 +366,13 @@ Command(
               });
               let inTribe = game.state.tribes[tribeIndex];
 
-              if (inTribe && inTribe.ownerSID == player.id) {
-                game.state.removeTribe(tribeIndex);
-                if (player.client) player.client.tribeJoinQueue = [];
-              } else {
-                game.state.leaveClan(player, tribeIndex);
+              if (inTribe) {
+                if (inTribe.ownerSID == player.id) {
+                  game.state.removeTribe(tribeIndex);
+                  if (player.client) player.client.tribeJoinQueue = [];
+                } else {
+                  game.state.leaveClan(player, tribeIndex);
+                }
               }
               game.state.joinClan(player, tribe);
             } else player.clanName = args.slice(3).join(" ");
