1ebdaef409c5ef36f0ed35972821756e989dae69
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-03-21 18:21:03 -0400

    remove

--- src/server.ts
+++ src/server.ts
@@ -50,7 +50,6 @@ function format(timestamp: number) {
 }
 
 app.use((req, res, next) => {
-  //if (req.headers.origin) res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
