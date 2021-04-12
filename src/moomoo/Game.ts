8ce848556e9a9bc7e8c426da0eccb2e97963107a
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-04-12 10:34:36 -0400

    add invinc to filter

--- src/moomoo/Game.ts
+++ src/moomoo/Game.ts
@@ -1398,7 +1398,7 @@ export default class Game {
       if (this.spikeAdvance > 6800) {
         this.close(
           `Game Finished<br>Winners: ${this.state.players
-            .filter((p) => !p.dead)
+            .filter((p) => !p.dead && !p.invincible)
             .map((p) => p.name)
             .join(", ")}`,
           -1
