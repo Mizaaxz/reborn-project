88814070ae81675415e90113a99b60d0a15b4ffa
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-10-07 21:13:26 -0400

    add port config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,11 @@
+import { readFileSync } from "fs";
+
+let PORT = 80;
+
+try {
+  PORT = Number(String(readFileSync(__dirname + "/../PORT")));
+} catch (e) {}
+
 const config = {
   allowedMax:
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !@#$%^&*()_+-=[]|:;\\\"',<.>/?`~{}",
@@ -11,10 +19,13 @@ const config = {
   featuredYT: [
     { name: "Meow", link: "https://www.youtube.com/channel/UCWANi1TTqUP1ar4VTlOrqDA" },
     { name: "Riseking", link: "https://www.youtube.com/channel/UCe3BeaOhhFLYEWPq62Anaxw" },
-	{ name: "AppleTacoYT", link: "https://www.youtube.com/channel/UCXd0WfGLoHVyx82AKB4ZSHQ" },
-	{ name: "21st - Your Local Idiot", link: "https://www.youtube.com/channel/UCPtSORLvl44enwpHHXHpreQ" },
-	{ name: "123SMG", link: "https://www.youtube.com/channel/UCt96Ef3O4OhzWk2LLYb7ShA" },
-	{ name: "Dashre", link: "https://www.youtube.com/channel/UCgr3LL4BM_xWGMEWSizf1VQ"}
+    { name: "AppleTacoYT", link: "https://www.youtube.com/channel/UCXd0WfGLoHVyx82AKB4ZSHQ" },
+    {
+      name: "21st - Your Local Idiot",
+      link: "https://www.youtube.com/channel/UCPtSORLvl44enwpHHXHpreQ",
+    },
+    { name: "123SMG", link: "https://www.youtube.com/channel/UCt96Ef3O4OhzWk2LLYb7ShA" },
+    { name: "Dashre", link: "https://www.youtube.com/channel/UCgr3LL4BM_xWGMEWSizf1VQ" },
   ],
   gameObjectNearbyRadius: 1250,
   maxCPS: "25",
@@ -22,7 +33,7 @@ const config = {
   passwordLength: { min: 8, max: 30 },
   playerNearbyRadius: 1250,
   animalNearbyRadius: 1250,
-  port: 80,
+  port: PORT,
   prefix: "!",
   usernameLength: { min: 4, max: 16 },
   maxAge: 100,
