80e27ddcfc74d8edc0968950208446e9bdd79e0f
Meow VPS <undefined@null.com>
2021-12-22 00:03:43 +0000

    account cache

--- src/moomoo/Account.ts
+++ src/moomoo/Account.ts
@@ -14,8 +14,14 @@ type Account = {
   createdAt?: number;
 };
 
-export function getAccount(username: string) {
-  return db.get(`account_${username.replace(/ /g, "+")}`) as Account;
+let AccountCache: Account[] = [];
+
+export function getAccount(username: string): Account {
+  let found = AccountCache.find((a) => a.username == username);
+  if (found) return found;
+  let acc = db.get(`account_${username.replace(/ /g, "+")}`) as Account;
+  AccountCache.push(acc);
+  return getAccount(username);
 }
 export function setAccount(username: string, acc: Account) {
   return db.set(`account_${username.replace(/ /g, "+")}`, acc);
