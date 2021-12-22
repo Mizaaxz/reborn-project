aa33f2cb17201ba418970ed530ffcfa6c98b0bd9
Meow VPS <undefined@null.com>
2021-12-22 01:23:20 +0000

    remove

--- src/config.ts
+++ src/config.ts
@@ -23,18 +23,6 @@ const config = {
       link: "https://www.youtube.com/channel/UCWANi1TTqUP1ar4VTlOrqDA",
     },
     {
-      name: "Riseking",
-      link: "https://www.youtube.com/channel/UCe3BeaOhhFLYEWPq62Anaxw",
-    },
-    {
-      name: "AppleTacoYT",
-      link: "https://www.youtube.com/channel/UCXd0WfGLoHVyx82AKB4ZSHQ",
-    },
-    {
-      name: "21st - Your Local Idiot",
-      link: "https://www.youtube.com/channel/UCPtSORLvl44enwpHHXHpreQ",
-    },
-    {
       name: "123SMG",
       link: "https://www.youtube.com/channel/UCt96Ef3O4OhzWk2LLYb7ShA",
     },
