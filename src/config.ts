caf5e20a56a214e304e736731ab5dcc408451aec
Meow VPS <undefined@null.com>
2022-01-08 15:53:55 +0000

    fix

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ try {
 } catch (e) {}
 
 const config = {
-  version: "1.16.11b",
+  version: "1.14.11b",
   allowedMax:
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !@#$%^&*()_+-=[]|:;\\\"',<.>/?`~{}",
   allowedPassword:
